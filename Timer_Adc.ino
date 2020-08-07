f8826a4bca80e9ca9cc70e25339fe9208dc98fa3
shuvangkar <sshuvo93@gmail.com>
2020-08-08 03:25:09 +0600

    library is wokring in simulation

--- Timer_Adc.ino
+++ Timer_Adc.ino
@@ -1,22 +1,25 @@
 #include "Timer1.h"
 
+int adc_result = 0;
+
 void setup() 
 {
- Serial.begin(9600);
+ Serial.begin(57600);
  Serial.println(F("Setup Done"));
- timer1.initialize(1); //1KHz timer
+ timer1.initialize(100); //1Hz timer
  timer1.attachIntCompB(timerIsr);
  timer1.start();
  adc.begin();
  adc.attachInterrupt(adcIsr);
  adc.setAutoTriggerSource(TIMER1_CMP_B);
  adc.startConversion(0);
- DDRB |= 1<<PB5;
+ DDRB |= 1<<PB5; //Arduino default LED 13
+ 
 }
 
 void loop() 
 {
-  
+ Serial.println(adc_result); 
 }
 void timerIsr()
 {
@@ -25,6 +28,7 @@ void timerIsr()
 
 void adcIsr()
 {
+  adc_result = ADCW;
   PORTB ^= 1<<PB5;
 }
 
